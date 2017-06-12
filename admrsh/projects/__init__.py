14b0aab376f2612e9f4893e2009d96469eefb760
Derek Marsh <dmarsh@southernco.com>
2017-06-12 09:52:44 -0400

    Fixes for projects blueprint -absolute static paths in template.html so nginx always serves from the correct source. -div content on projects.html

--- admrsh/projects/__init__.py
+++ admrsh/projects/__init__.py
@@ -4,8 +4,6 @@ from flask import Blueprint
 
 projects = Blueprint('projects',
                      __name__,
-                     template_folder='templates')#,
-                     #static_folder='static',
-                     #static_url_path='/static/base')
+                     template_folder='templates')
 
 from . import views
