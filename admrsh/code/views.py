59206df42f0432415ad22c6edcb64c34904fe32a
Derek Marsh <dmarsh@southernco.com>
2017-06-28 11:43:23 -0400

    Remove infrastructure from code sample.

--- admrsh/code/views.py
+++ admrsh/code/views.py
@@ -5,11 +5,6 @@ from flask import render_template
 from . import code
 
 
-@code.route('/infrastructure')
-def infrastructure():
-    return render_template('infrastructure.html', title='Infrastructure')
-
-
 @code.route('/socketchat')
 def socket_chat():
     return render_template('socket_chat.html', title='Socket Chat')
