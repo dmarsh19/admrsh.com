56fa7d87960e3a519e84f1412acf05c648d567f9
Derek Marsh <dmarsh@southernco.com>
2017-06-28 11:39:48 -0400

    Contact at base of home, no longer separate page.

--- admrsh/base/views.py
+++ admrsh/base/views.py
@@ -8,8 +8,3 @@ from . import base
 @base.route('/')
 def home():
     return render_template('home.html', title='Home')
-
-
-@base.route('/contact')
-def contact():
-    return render_template('contact.html', title='Contact')
