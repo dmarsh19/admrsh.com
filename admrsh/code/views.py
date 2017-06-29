6d09137810c44a8683bb627487ec773002e2f528
Derek Marsh <dmarsh@southernco.com>
2017-06-29 16:50:34 -0400

    Add basics for atmo

--- admrsh/code/views.py
+++ admrsh/code/views.py
@@ -8,3 +8,8 @@ from . import code
 @code.route('/socketchat')
 def socket_chat():
     return render_template('socket_chat.html', title='Socket Chat')
+
+
+@code.route('/atmo')
+def atmo():
+    render_template('atmo.html', title='atmo')
