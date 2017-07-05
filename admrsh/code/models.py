1693c90bad771cad4c41312bdbe6bdc0341b0ae4
Derek Marsh <dmarsh@southernco.com>
2017-07-05 15:46:43 -0400

    atmo model working.

--- admrsh/code/models.py
+++ admrsh/code/models.py
@@ -3,9 +3,11 @@ from admrsh import db
 
 class AtmoReading(db.Model):
     """."""
+    __tablename__ = 'DHT'
+    id = db.Column('oid', db.Integer, primary_key=True)
     temp = db.Column(db.Float)
     humd = db.Column(db.Float)
-    datetime = db.Column(db.DateTime)
+    datetime = db.Column(db.Text)
 
     def __init__(self, temp, humd, datetime):
         self.temp = temp
