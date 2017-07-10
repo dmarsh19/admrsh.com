fb00335e10f4a87f54730357fed21572231f608b
Derek Marsh <dmarsh@southernco.com>
2017-07-10 11:25:56 -0400

    separate atmo js to own file.

---
+++ admrsh/code/static/js/atmo.js
@@ -0,0 +1,50 @@
+var chartData = {
+    labels: [],
+    datasets: [{
+    label: 'temperature',
+    data: [],
+    fill: false,
+    // line color; red, opacity: 40%
+    borderColor: "rgba(255, 0, 0, 0.4)",
+    pointBorderColor: "rgba(255, 0, 0, 0.6)",
+    pointBackgroundColor: "rgba(255, 0, 0, 0.6)",
+    pointHoverBorderColor: "rgba(255, 0, 0, 1)",
+    pointHoverBackgroundColor: "rgba(255, 0, 0, 1)"
+    },
+    {
+    label: 'humidity',
+    data: [],
+    fill: false,
+    // line color; blue, opacity: 40%
+    borderColor: "rgba(0, 0, 255, 0.4)",
+    pointBorderColor: "rgba(0, 0, 255, 0.6)",
+    pointBackgroundColor: "rgba(0, 0, 255, 0.6)",
+    pointHoverBorderColor: "rgba(0, 0, 255, 1)",
+    pointHoverBackgroundColor: "rgba(0, 0, 255, 1)"
+    }]
+};
+
+var chartOptions = {
+    animation: {
+        duration: 0 // general animation time
+    },
+    hover: {
+        animationDuration: 0 // duration of animations when hovering an item
+    },
+    responsiveAnimationDuration: 0, // animation duration after a resize
+    elements: {
+        line: {
+            tension: 0 // disables bezier curves
+        }
+    }
+};
+
+function asyncDrawChart(apiUrl, lineChart) {
+    $.get( apiUrl ).done(function( atmoData ) {
+        //console.log( atmoData.map(i => i['datetime']) );
+        lineChart.data.labels = atmoData.map(function(i){ return i['datetime'] });
+        lineChart.data.datasets[0].data = atmoData.map(function(i){ return i['temp'] });
+        lineChart.data.datasets[1].data = atmoData.map(function(i){ return i['humd'] });
+        lineChart.update();
+    });
+};
